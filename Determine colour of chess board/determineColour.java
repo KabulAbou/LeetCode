8a41a748c9d5fb1de7a6dd863fd51d6aeebbcfae
kareem <kareem.ali.abou-sena@sap.com>
2024-06-12 15:48:16 +0200

    final push

--- Determine colour of chess board/determineColour.java
+++ Determine colour of chess board/determineColour.java
@@ -3,7 +3,11 @@ class Solution {
         if(Math.toIntExact(coordinates.charAt(1))% 2 != 0 && (coordinates.charAt(0) == 'a' 
         || coordinates.charAt(0) == 'c' || coordinates.charAt(0) == 'e' || coordinates.charAt(0) == 'g')) {
             return false;
-        } else {
+        } else if (Math.toIntExact(coordinates.charAt(1))% 2 == 0 && (coordinates.charAt(0) == 'b' 
+        || coordinates.charAt(0) == 'd' || coordinates.charAt(0) == 'f' || coordinates.charAt(0) == 'h')){
+            return false;
+        }
+        else {
             return true;
         }
     }
